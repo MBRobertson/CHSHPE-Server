8369fe6d00a02635071fa7d76eae0925abf678f2
Michael Robertson <mbr.robertson@gmail.com>
2016-10-11 03:47:19 +0000

    Please work now timezones!

--- api/day.js
+++ api/day.js
@@ -4,7 +4,7 @@ var Config = require('./db/config');
 
 moment.tz.setDefault("NZ");
 
-var periods = {
+var periodsold = {
     1: {
         start: moment.tz("8:30am", "h:mma", "NZ"),
         end: moment.tz("9:35am", "h:mma", "NZ")
@@ -27,6 +27,29 @@ var periods = {
     }
 }
 
+var periods = {
+    1: {
+        start: { hour: 8, minute: 30 },
+        end:{ hour: 9, minute: 30 }
+    },
+    2: {
+        start: { hour: 9, minute: 30 },
+        end:{ hour: 10, minute: 40 }
+    },
+    3: {
+        start: { hour: 10, minute: 40 },
+        end:{ hour: 12, minute: 25 }
+    },
+    4: {
+        start: { hour: 12, minute: 25 },
+        end:{ hour: 13, minute: 30 }
+    },
+    5: {
+        start: { hour: 13, minute: 30 },
+        end:{ hour: 15, minute: 15 }
+    }
+}
+
 var getDay = function(weekAEven) {
     var now = moment.tz('NZ');
     var reference = weekAEven ? 2 : 1
@@ -36,26 +59,36 @@ var getDay = function(weekAEven) {
 
 var getPeriod = function() {
     var now = moment.tz('NZ');
+    var start = moment.tz('NZ');
+    var end = moment.tz('NZ');
     for (var p = 1; p <= 5; p++) {
-        if (now.isBetween(periods[p].start, periods[p].end)) {
+        start.set(periods[p].start);
+        end.set(periods[p].end);
+        if (now.isBetween(start, end)) {
             return p;
         }
+        
+        
     }
-    if (now.isAfter(periods[5].end)) return -1;
+    //end.set()
+    if (now.isAfter(end)) return -1;
     return 0;
 }
 var getTime = function(weekAEven) {
     var day = getDay(weekAEven);
     var now = moment.tz('NZ');
-    var now2 = periods[3].start;
-    var now3 = periods[3].end;
+    var now2 = moment.tz('NZ');
+    var now3 = moment.tz('NZ');
+    now2.set(periods[5].start)
+    now3.set(periods[5].end)
     return {
         week: day.week,
         day: day.day,
         period: getPeriod(),
         time: now.format(),
         now2: now2.format(),
-        now3: now3.format()
+        now3: now3.format(),
+        after: now.isAfter(now3)
     }
 }
 
